c930b4f06f31f2f760d17c697789b6b2279e8d4d
mauriciofollonier <mauriciodamianfollonier@hotmail.com>
2024-11-13 23:18:16 -0300

    Create an server where show an app similar to Twitter

---
+++ src/server/server.js
@@ -0,0 +1,27 @@
+//path es una propiedad de Node que permite encontrar carpetas en el proyecto
+const path = require('path');
+const express = require('express');
+
+const startServer = (options) => {
+    //Estos datos lo traemos de app.js y lo desesctruturamos acá
+    const { port, public_path = 'public' } = options;
+    const app = express();
+
+    // Para poder usar middlewares "use"(express)
+    // Contenido estático que ponemos disponible
+    app.use(express.static(public_path));
+
+    app.get('*', (req, res) => {
+        const indexPath = path.join(__dirname + `../../../${public_path}/index.html`);
+        res.sendFile(indexPath);
+    })
+
+    app.listen(port, () => {
+        console.log(`Escuchando en el puerto ${port}`)
+    })
+    
+}
+
+module.exports = {
+    startServer
+}
